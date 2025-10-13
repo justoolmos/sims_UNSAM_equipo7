172ab515baa6e331dcbe34b957280460ae609a72
justo desktop <justo.olmos388@gmail.com>
2025-10-13 09:35:46 -0300

    versiones finales de ising_mc y main, resultados tiene el calculo de autocorrelacion usando corr.dat

--- TP_MC/main.f90
+++ TP_MC/main.f90
@@ -4,7 +4,7 @@ program MC
         use ziggurat
         implicit none
         integer, parameter :: dp = selected_real_kind(15, 307)
-        real(kind=dp) :: T, r, E_tot, E_med, E_sqr, M_tot, M_med, M_sqr, dE, beta
+        real(kind=dp) :: T, r, E_tot, E_med, E_sqr, M_tot, M_med, M_sqr, dE, beta, acept
         integer :: N, x, y, n_steps,s_k,i
         integer, allocatable :: A(:,:)
            
@@ -17,19 +17,18 @@ program MC
 
         allocate(A(0:N+1,0:N+1))  !A tiene que ser alocado antes de get_matrix_rand o de load_matrix
         !call get_matrix_rand(A,N) !NxN 1 y -1 asignados aleatoriamente
-        !call save_matrix(A,'matriz_inicial.dat')
         call load_matrix(A,N,'matriz1.dat')   !opcionalmente carga la matriz desde un archivo
-        !call print_matrix_full(A)
         E_tot = get_total_energy(A)   !energia computada con una matriz entera
         E_med = 0
         E_sqr = 0
         M_tot = get_total_mag(A)      !magnetizacion desde una matriz entera
         M_med = 0
         M_sqr = 0
+        acept = 0
         
         open(unit=30, file="matrix_out.dat", status = "new", action="write")
         open(unit = 10, file="out.dat", status = "replace", action = "write")
-        write(10,*) "Paso,E_tot,E_med,E_sqr,M_tot,M_med,M_sqr"
+        write(10,*)"Paso,E_tot,E_med,E_sqr,M_tot,M_med,M_sqr,acept_ratio"
         do i=1,n_steps  
                 x = int(uni()*N)+1  !elegimos una posicion aleatoria
                 y = int(uni()*N)+1
@@ -41,12 +40,14 @@ program MC
                         A(x,y) = -s_k
                         E_tot = E_tot + dE
                         M_tot = M_tot + (-2*s_k)
+                        acept = acept + 1
                 else
                         r = uni()
                         if(r < exp(-beta*(dE))) then
                                 A(x,y) = -s_k
                                 E_tot = E_tot + dE
                                 M_tot = M_tot + (-2*s_k)
+                                acept = acept + 1
                         end if
                 end if
                 !actualizamos las filas y columnas de condicion periodica
@@ -59,11 +60,11 @@ program MC
                 M_med = M_med + M_tot
                 E_sqr = E_sqr + (E_tot*E_tot)
                 M_sqr = M_sqr + (M_tot*M_tot)
-                if (MOD(i,100) == 0) then       
+                if (MOD(i,10000) == 0) then       
                         call save_matrix(A,'matriz1.dat')  !guardar la matriz
-                        write(10,*) i,",",E_tot,",",E_med/i,",",E_sqr/i,",",M_tot,",",M_med/i,",",M_sqr/i    ! !se guardan la energia actual y el promedio hasta este paso, etc 
+                        write(10,*) i,",",E_tot,",",E_med/i,",",E_sqr/i,",",M_tot,",",M_med/i,",",M_sqr/i,",",acept/i    ! !se guardan la energia actual y el promedio hasta este paso, etc 
                 end if
-                if(mod(i,1000)==0) then
+                if(mod(i,10000)==0) then
                         write(30,*) A
                 end if
         end do
